dbb4a7c7dbec540e9f5e86d7a26c31258bf40190
Talha <personaagent37@gmail.com>
2026-08-13 20:26:53 +0530

    Fix SMTP by adding fallback credentials for Render

--- backend/server.js
+++ backend/server.js
@@ -114,8 +114,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.GMAIL_ADDRESS,
-    pass: process.env.GMAIL_APP_PASSWORD
+    user: process.env.GMAIL_ADDRESS || 'codingpython57@gmail.com',
+    pass: process.env.GMAIL_APP_PASSWORD || 'pngm pgrb dyzi izzl'
   }
 });
 
